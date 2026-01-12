ddfa3ba9464fb46d19d5629ad9d3dcddc2a25d04
stanley adigwe <adigwestanley>
2026-01-12 08:22:46 -0700

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Stanley Adigwe on 2026-01-12.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
